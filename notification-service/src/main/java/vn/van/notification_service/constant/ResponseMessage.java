c53e6ec99c3fe51149b6079ef0b9ac914c8492e7
To Dinh Van <todinhvan2608@gmail.com>
2025-12-27 10:25:53 +0700

    Initial Notification Service and connect to mongodb-server through docker

---
+++ notification-service/src/main/java/vn/van/notification_service/constant/ResponseMessage.java
@@ -0,0 +1,35 @@
+package vn.van.notification_service.constant;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+
+@Getter
+public enum ResponseMessage {
+    // Success
+
+    // Error
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error"),
+    INVALID_KEY_RESPONSE_MESSAGE(HttpStatus.BAD_REQUEST, "Invalid Key Response"),
+    // Error field
+
+    ;
+
+    private final int statusCode;
+    private final HttpStatus status;
+    private final String message;
+
+    ResponseMessage(String message) {
+        this(HttpStatus.UNPROCESSABLE_ENTITY.value(), HttpStatus.UNPROCESSABLE_ENTITY, message);
+    }
+
+    ResponseMessage(HttpStatus status, String message) {
+        this(status.value(), status, message);
+    }
+
+    ResponseMessage(int statusCode, HttpStatus status, String message) {
+        this.statusCode = statusCode;
+        this.status = status;
+        this.message = message;
+    }
+}
---
+++ notification-service/src/main/java/vn/van/notification_service/exception/GlobalExceptionHandler.java
@@ -0,0 +1,67 @@
+package vn.van.notification_service.exception;
+
+import java.util.Map;
+import java.util.Objects;
+
+import jakarta.validation.ConstraintViolation;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import lombok.extern.slf4j.Slf4j;
+import vn.van.notification_service.constant.ResponseMessage;
+import vn.van.notification_service.dto.response.ApiResponse;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<ApiResponse<?>> handleException(Exception e) {
+        log.error(e.toString());
+        ResponseMessage responseMessage = ResponseMessage.INTERNAL_SERVER_ERROR;
+        ApiResponse<?> response = toApiResponse(responseMessage, null);
+        return new ResponseEntity<>(response, responseMessage.getStatus());
+    }
+
+    @ExceptionHandler(value = ApplicationException.class)
+    public ResponseEntity<ApiResponse<?>> handleApplicationException(ApplicationException e) {
+        log.error(e.toString());
+        ResponseMessage responseMessage = e.getResponseMessage();
+        ApiResponse<?> response = toApiResponse(responseMessage, null);
+        return new ResponseEntity<>(response, responseMessage.getStatus());
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<?>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error(e.toString());
+        ResponseMessage responseMessage = ResponseMessage.INVALID_KEY_RESPONSE_MESSAGE;
+        String message = responseMessage.getMessage();
+
+        try {
+            responseMessage = ResponseMessage.valueOf(e.getFieldError().getDefaultMessage());
+            var violation = e.getBindingResult().getAllErrors().getFirst().unwrap(ConstraintViolation.class);
+            var attributes = violation.getConstraintDescriptor().getAttributes();
+            message = mapCustomMessage(responseMessage.getMessage(), attributes);
+        } catch (IllegalArgumentException ex) {
+        }
+
+        ApiResponse<?> response = toApiResponse(responseMessage, message);
+        return new ResponseEntity<>(response, responseMessage.getStatus());
+    }
+
+    private <T> ApiResponse<T> toApiResponse(ResponseMessage responseMessage, String customMessage) {
+        return ApiResponse.<T>builder()
+                .statusCode(responseMessage.getStatusCode())
+                .status(responseMessage.getStatus())
+                .message(Objects.isNull(customMessage) ? responseMessage.getMessage() : customMessage)
+                .build();
+    }
+
+    private String mapCustomMessage(String message, Map<String, Object> attributes) {
+        return attributes.containsKey("minAge")
+                ? message.replace("{minAge}", attributes.get("minAge").toString())
+                : message;
+    }
+}
