b817f659a7e17248c83eb93b4f8abc72de38f525
To Dinh Van <todinhvan2608@gmail.com>
2025-12-30 09:43:35 +0700

    Initial Chat Service and config security for authorization

---
+++ chat-service/src/main/java/vn/van/chat_service/config/CustomJwtDecoder.java
@@ -0,0 +1,29 @@
+package vn.van.chat_service.config;
+
+import com.nimbusds.jwt.SignedJWT;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtException;
+import org.springframework.stereotype.Component;
+
+import java.text.ParseException;
+import java.time.Instant;
+import java.util.Map;
+
+@Component
+public class CustomJwtDecoder implements JwtDecoder {
+    @Override
+    public Jwt decode(String token) throws JwtException {
+        try {
+            SignedJWT signedJWT = SignedJWT.parse(token);
+            Instant issuedAt = signedJWT.getJWTClaimsSet().getIssueTime().toInstant();
+            Instant expiresAt = signedJWT.getJWTClaimsSet().getExpirationTime().toInstant();
+            Map<String, Object> headers = signedJWT.getHeader().toJSONObject();
+            Map<String, Object> claims = signedJWT.getJWTClaimsSet().getClaims();
+
+            return new Jwt(token, issuedAt, expiresAt, headers, claims);
+        } catch (ParseException e) {
+            throw new JwtException("Token invalid");
+        }
+    }
+}
---
+++ chat-service/src/main/java/vn/van/chat_service/dto/response/ApiResponse.java
@@ -0,0 +1,20 @@
+package vn.van.chat_service.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse<T> {
+    int statusCode;
+    HttpStatus status;
+    String message;
+    T data;
+    T error;
+}
