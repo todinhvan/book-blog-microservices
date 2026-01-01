81bafeb48d118ac9aa39cb06f22782b8881b0e00
To Dinh Van <todinhvan2608@gmail.com>
2026-01-01 23:51:47 +0700

    Initial Spring AI Service and config api key with gemini

---
+++ spring-ai-service/src/main/java/vn/van/spring_ai_service/SpringAiServiceApplication.java
@@ -0,0 +1,13 @@
+package vn.van.spring_ai_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiServiceApplication.class, args);
+	}
+
+}
