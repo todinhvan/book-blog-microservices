919ad2f8a9a94ee1ed3420ac9d8b5ed83e5f5c2f
To Dinh Van <todinhvan2608@gmail.com>
2026-01-02 08:42:02 +0700

    Build an effective prompt with system messages

--- spring-ai-service/src/main/java/vn/van/spring_ai_service/dto/request/ChatRequest.java
+++ spring-ai-service/src/main/java/vn/van/spring_ai_service/dto/request/ChatRequest.java
@@ -1,13 +1,3 @@
 package vn.van.spring_ai_service.dto.request;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.experimental.FieldDefaults;
-
-@Getter
-@Setter
-@FieldDefaults(level = AccessLevel.PRIVATE)
-public class ChatRequest {
-    String message;
-}
+public record ChatRequest(String message) {}
--- spring-ai-service/src/main/java/vn/van/spring_ai_service/service/impl/ChatServiceImpl.java
+++ spring-ai-service/src/main/java/vn/van/spring_ai_service/service/impl/ChatServiceImpl.java
@@ -4,6 +4,9 @@ import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.stereotype.Service;
 import vn.van.spring_ai_service.dto.request.ChatRequest;
 import vn.van.spring_ai_service.service.ChatService;
@@ -20,6 +23,15 @@ public class ChatServiceImpl implements ChatService {
 
     @Override
     public String chat(ChatRequest request) {
-        return chatClient.prompt(request.getMessage()).call().content();
+        SystemMessage systemMessage = new SystemMessage("""
+                You are VanAI
+                You should response with a formal voice
+                """);
+        UserMessage userMessage = new UserMessage(request.message());
+        Prompt prompt = new Prompt(systemMessage, userMessage);
+
+        return chatClient
+                .prompt(prompt)
+                .call().content();
     }
 }
